ac78eedc50e04d1a30625d80f836b115c2f6155a
Paarija <parijanath7@gmail.com>
2025-09-07 12:21:42 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Medical-Chatbot-with-LLMs
+
+
+
+
+### STEP 01 - Create a conda repository
+
+```bash
+conda create -n medibot python=3.10 -y
+```
+
+### STEP 02 - install the requirements
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Parija Nath",
+    author_email="parijanath7@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
